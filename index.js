a3be93b7572942061b6692b263c9d44f5df1f414
tikrack <tikrackcode@gmail.com>
2025-03-29 19:53:26 +0330

    new - base and new version

---
+++ index.js
@@ -0,0 +1,19 @@
+// Please install OpenAI SDK first: `npm install openai`
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+        baseURL: 'https://api.deepseek.com/v1',
+        apiKey: 'sk-f52bdcfa85f243ea991e0b54af317d1b'
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "deepseek-chat",
+  });
+
+  console.log(completion.choices[0].message.content);
+}
+
+main();
